8edd3485b3640003de17c8392e8b6f8031f22bd9
joao <joaobrito.eng@gmail.com>
2017-12-16 00:19:36 +0000

    wip

--- server.js
+++ server.js
@@ -103,7 +103,7 @@ apiRoutes.post('/authenticate', function(req, res) {
 apiRoutes.use(function(req, res, next) {
 
 	// check header or url parameters or post parameters for token
-	var token = req.body.token || req.param('token') || req.headers['x-access-token'] || req.headers['Bearer'];
+    var token = req.body.token || req.param('token') || req.headers['x-access-token'] || req.headers['token'];
 
 	// decode token
 	if (token) {
